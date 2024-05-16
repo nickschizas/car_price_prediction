c94db59c5c996b0f1ce9121cb15a8e3b49336ef5
Nick Schizas <100963244+nickschizas@users.noreply.github.com>
2024-05-17 00:41:05 +0300

    Update Car_Price_Prediction.py

--- Car_Price_Prediction.py
+++ Car_Price_Prediction.py
@@ -65,6 +65,7 @@ def train_fit_test():
     model.fit(X_train, y_train)
     # Gather model metrics
     y_pred = model.predict(X_test)
+    from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
     mse = mean_squared_error(y_test, y_pred)
     mae = mean_absolute_error(y_test, y_pred)
     r2 = r2_score(y_test, y_pred)    
