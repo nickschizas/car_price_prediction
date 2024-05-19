ffd6f798df7f55c40579e65d8381110f30b906ee
Nick Schizas <100963244+nickschizas@users.noreply.github.com>
2024-05-19 18:15:40 +0300

    Update 3_Model.py

--- pages/3_Model.py
+++ pages/3_Model.py
@@ -7,3 +7,10 @@ st.session_state.model_stats
 metric_col = st.columns(4)
 with metric_col[0]:
   st.metric(label='R-squared', value="{:.2%}".format(st.session_state.model_stats['r2']))
+with metric_col[1]:
+  st.metric(label='Mean Absolute Error', value="{:,.2f}".format(st.session_state.model_stats['mae']))
+with metric_col[2]:
+  st.metric(label='Residuals Mean', value="{:,.2f}".format(st.session_state.model_stats['res_mean']))
+with metric_col[0]:
+  st.metric(label='Residuals Standard Deviation', value="{:,.2f}".format(st.session_state.model_stats['res_std']))
+
