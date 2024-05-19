070e0d9583e96eb6f97974cad85e735b9ad1ca90
Nick Schizas <100963244+nickschizas@users.noreply.github.com>
2024-05-19 16:21:53 +0300

    Update 3_Model.py

--- pages/3_Model.py
+++ pages/3_Model.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.set_page_config(layout="wide", page_title='Model', page_icon=':robot_face:')
 
-st.markdown(':construction: Under Construction :construction:')
+st.session_state.model_stats
