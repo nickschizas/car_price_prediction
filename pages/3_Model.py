222026b54f2abe498a7173e5dbfc3c78d609b45e
Nick Schizas <100963244+nickschizas@users.noreply.github.com>
2024-05-19 18:59:27 +0300

    Update 3_Model.py

--- pages/3_Model.py
+++ pages/3_Model.py
@@ -7,21 +7,14 @@ st.set_page_config(layout="wide", page_title='Model', page_icon=':robot_face:')
 
 text = f"""
 Model used for prediction is an untuned Random Forest Regressor.<br>
-**Model Stats**<br>
-R-squared: {"{:.2%}".format(st.session_state.model_stats['r2'])}
+<b>Model Stats<b>:<br>
+R-squared: {"{:.2%}".format(st.session_state.model_stats['r2'])}<br>
+Mean Absolute Error: {"{:,.2f}".format(st.session_state.model_stats['mae'])}<br>
+Residuals Mean: {"{:,.2f}".format(st.session_state.model_stats['res_mean'])}<br>
+Residuals Std: {"{:,.2f}".format(st.session_state.model_stats['res_std'])}
 """
 
 st.sidebar.markdown(f'<em>{text}<em>', unsafe_allow_html=True)
-
-metric_col = st.columns(4)
-with metric_col[0]:
-  st.metric(label='R-squared', value="{:.2%}".format(st.session_state.model_stats['r2']))
-with metric_col[1]:
-  st.metric(label='Mean Absolute Error', value="{:,.2f}".format(st.session_state.model_stats['mae']))
-with metric_col[2]:
-  st.metric(label='Residuals Mean', value="{:,.2f}".format(st.session_state.model_stats['res_mean']))
-with metric_col[3]:
-  st.metric(label='Residuals Standard Deviation', value="{:,.2f}".format(st.session_state.model_stats['res_std']))
 
 residuals = st.session_state.model_stats['residuals']
 fig = make_subplots(rows=1, cols=2, subplot_titles=('Residuals Plot', 'Residuals Histogram'))
