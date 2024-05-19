c5f0ca89470ed93c239df6f86dc62cfc19a02070
Nick Schizas <100963244+nickschizas@users.noreply.github.com>
2024-05-19 18:13:47 +0300

    Update 3_Model.py

--- pages/3_Model.py
+++ pages/3_Model.py
@@ -3,3 +3,7 @@ import streamlit as st
 st.set_page_config(layout="wide", page_title='Model', page_icon=':robot_face:')
 
 st.session_state.model_stats
+
+metric_col = st.columns(4)
+with metric_col[0]:
+  st.metric(label='R-squared', value="{:.2%}".format(st.session_state.model_stats['r2'])
